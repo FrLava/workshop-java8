d2c42d10370de80cfa8647463a06196cc58cf67e
Franc LAVAUD <franc.lavaud@gmail.com>
2017-04-12 16:54:17 +0200

    Commit sans tp 3 fait

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -77,7 +77,7 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, p->(p.getAge()>49 && DigestUtils.sha512Hex(p.getPassword()).equals("ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff")));
+        List<Person> result = filter(personList, p->(p.getAge()>49 && DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex)));
 
         assertThat(result.size(), is(6));
         assertThat(result, everyItem(hasProperty("password", is("test"))));
