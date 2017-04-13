d681da9b67e3762f4fe0a4362c7b84b514ccc3c1
Franc LAVAUD <franc.lavaud@gmail.com>
2017-04-13 11:20:33 +0200

    Commit stream tp 3 exo 2 sur 4

--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.Map;
 import static java.util.stream.Collectors.*;
 
+import static java.util.stream.Collectors.*;
+
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
@@ -26,7 +28,10 @@ public class Stream_03_Test {
         List<Customer> customers = new Data().getCustomers();
 
         // TODO construire une chaîne contenant les prénoms des clients triés et séparé par le caractère "|"
-        String result = null;
+        String result = customers.stream()
+        		.map(Customer::getFirstname)
+        		.sorted()
+        		.collect(joining("|"));
 
         assertThat(result, is("Alexandra|Cyril|Johnny|Marion|Sophie"));
     }
